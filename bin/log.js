aa9a3200fe93211026d0e498fc84a8ed32a34792
hejianglin <hejianglin@uino.com>
2023-04-12 11:08:57 +0800

    🎈 perf: Added Log Daily log splitting

--- bin/log.js
+++ bin/log.js
@@ -1,22 +1,23 @@
 /* createLogger  */
-const { format, createLogger, transports } = require('winston')
+const { format, createLogger } = require('winston')
 const path = require('path')
-const logPath = path.join(process.cwd(), 'logs/server.log')
+const DailyRotateFile = require('winston-daily-rotate-file')
+
+const logPath = path.join(process.cwd(), 'logs')
+
 const log = (() => {
     let logger
     if (!logger) {
         logger = createLogger({
             transports: [
-                new transports.File({
-                    filename: logPath,
+                new DailyRotateFile({
+                    filename: path.join(logPath, 'server_%DATE%.log'),
+                    datePattern: 'YYYY-MM-DD',
                     level: 'info',
                     format: format.combine(
-                        format.timestamp({ format: 'MMM-DD-YYYY HH:mm:ss' }),
+                        format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
                         format.align(),
-                        format.printf(
-                            (info) =>
-                                `${info.level}: ${[info.timestamp]}: ${info.message}`,
-                        ),
+                        format.printf((info) => `${info.level}: ${[info.timestamp]}: ${info.message}`),
                     ),
                 }),
             ],

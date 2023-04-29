6162b6bf59ab6288d149879dc59aeb1a0af4b722
helsonlin <helsonlin@163.com>
2023-04-29 12:33:45 +0800

    fix: fix windows platform download judge error

--- bin/utils/env.js
+++ bin/utils/env.js
@@ -32,7 +32,7 @@ const downloadFfmpeg = async (type) => {
         'linux-amd64': 'ffmpeg-4.4.1-linux-armel-32',
     }
     const suffix = typeLink[type]
-    const executableFileSuffix = typeLink[type].startsWith('win') ? 'ffmpeg.exe' : 'ffmpeg'
+    const executableFileSuffix = type.startsWith('win') ? 'ffmpeg.exe' : 'ffmpeg'
     const libPath = path.join(process.cwd(), `lib/${executableFileSuffix}`)
     const isExist = isFile(libPath)
     if (isExist) {
